8cdb7b24d6f438e96416a705b0bdec233e5caa37
jonathan briceño <brimanz2014@gmail.com>
2022-01-07 18:59:41 -0400

    first project using python

---
+++ crazy_histories/histories.py
@@ -0,0 +1,10 @@
+#using vars
+adj = input('Ingrese adjetivo por favor: ')
+verbo1 = input('Ingrese verbo 1 por favor: ')
+verbo2 = input('Ingrese verbo 2 por favor: ')
+sustantivo_plural = input('Ingrese sustantivo(plural) por favor: ')
+
+#crazy history.
+crazy_his = f"Programar es tan {adj}, Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con freecodecamp y alcanza tus {sustantivo_plural}"
+
+print(crazy_his)
